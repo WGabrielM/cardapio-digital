94c4f99086dbc107553c2c1fe95c93798e611c5c
Wellington Gabriel Monteiro Batista <wellingtongmb@gmail.com>
2023-10-30 11:07:17 -0300

    endind backend

--- cardapio/src/main/java/com/example/cardapio/controller/FoodController.java
+++ cardapio/src/main/java/com/example/cardapio/controller/FoodController.java
@@ -1,21 +1,32 @@
 package com.example.cardapio.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.cardapio.food.Food;
+import com.example.cardapio.food.FoodRepository;
+import com.example.cardapio.food.FoodResponseDTO;
+import com.example.cardapio.food.FoodRequestDTO;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("food")
+@RequestMapping("fooddb")
 public class FoodController {
+    @Autowired
+    private FoodRepository repository;
 
+    @CrossOrigin(origins = "*", allowedHeaders = "*")
+    @PostMapping
+    public void saveFood(@RequestBody FoodRequestDTO data){
+        Food foodData = new Food(data);
+        repository.save(foodData);
+    }
 
+    @CrossOrigin(origins = "*", allowedHeaders = "*")
     @GetMapping
-    public void getAll() {
-
-        List<Food> foodList = repository
-
+    public List<FoodResponseDTO> getAll() {
+        List<FoodResponseDTO> foodList = repository.findAll().stream().map(FoodResponseDTO::new).toList();
+        return foodList;
     }
 
 }
--- cardapio/src/main/java/com/example/cardapio/food/Food.java
+++ cardapio/src/main/java/com/example/cardapio/food/Food.java
@@ -2,15 +2,27 @@ package com.example.cardapio.food;
 
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Table(name = "foods")
 @Entity(name = "foods")
+@Getter //Gera todos os métodos de get em runtime
+@NoArgsConstructor //Declara um constructor que não tem argumentos
+@AllArgsConstructor //Aceita todos os argumentos do constructor
+@EqualsAndHashCode(of = "id") //Id é a representação única
 public class Food {
-
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     private String title;
     private String image;
     private Integer price;
 
+    public Food(FoodRequestDTO data){
+        this.image = data.image();
+        this.price = data.price();
+        this.title = data.title();
+    }
 }
