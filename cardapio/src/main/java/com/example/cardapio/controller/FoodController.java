a64e4029fa314d246b2f9af6d1e162d4d97a21bd
Wellington Gabriel Monteiro Batista <wellingtongmb@gmail.com>
2023-10-29 22:04:23 -0300

    init backend

---
+++ cardapio/src/main/java/com/example/cardapio/controller/FoodController.java
@@ -0,0 +1,21 @@
+package com.example.cardapio.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("food")
+public class FoodController {
+
+
+    @GetMapping
+    public void getAll() {
+
+        List<Food> foodList = repository
+
+    }
+
+}
