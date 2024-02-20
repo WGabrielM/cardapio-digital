7196abdf502a1f1a613781e011182de15846e260
Wellington Gabriel Monteiro Batista <wellingtongmb@gmail.com>
2024-02-19 21:18:33 -0300

    reset project

--- cardapio/src/main/java/com/example/cardapio/food/Food.java
+++ cardapio/src/main/java/com/example/cardapio/food/Food.java
@@ -1,28 +1,21 @@
 package com.example.cardapio.food;
 
-
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.EqualsAndHashCode;
-import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Table(name = "foods")
 @Entity(name = "foods")
-@Getter
-@NoArgsConstructor //Declara um constructor que não tem argumentos
-@AllArgsConstructor //Aceita todos os argumentos do constructor
-@EqualsAndHashCode(of = "id") //Id é a representação única
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(of = "id")
 public class Food {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long id;
+    private Long id;
     private String title;
     private String image;
     private Integer price;
-
-    public Food(FoodRequestDTO data){
-        this.image = data.image();
-        this.price = data.price();
-        this.title = data.title();
-    }
 }
