f474103ef5c75812894bd36d3c37631370a80876
Wellington Gabriel Monteiro Batista <wellingtongmb@gmail.com>
2024-05-31 11:56:38 -0300

    add hook for post useFoodDataMutate

---
+++ cardapio-frontend/src/hooks/useFoodDataMutate.ts
@@ -0,0 +1,24 @@
+import axios, { AxiosPromise } from "axios";
+import { FoodData } from "../interface/FoodData";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+
+const API_URL = 'http://localhost:8080';
+
+const postData = async (data: FoodData): AxiosPromise<any> => {
+    const response = axios.post(API_URL + '/food', data);
+    return response;
+}
+
+export function useFoodDataMutate() {
+    const queryClient = useQueryClient();
+    const mutate = useMutation({
+        retry: 2,
+        mutationFn: postData,
+        // Invalidate the oldest query and get the new one
+        onSuccess: () => {
+            queryClient.invalidateQueries(['food-data'])
+        }
+    })
+
+    return mutate;
+}
